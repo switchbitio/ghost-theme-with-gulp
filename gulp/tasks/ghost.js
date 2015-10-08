02e30e8d2a6ea0192872a10d04b74f3f469bc768
Donovan Muller <donovan.muller@gmail.com>
2015-10-08 23:54:12 +0200

    Part 1 - Running Ghost with Gulp

---
+++ gulp/tasks/ghost.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var ghost = require('ghost');
+var path = require('path');
+
+var g;
+
+gulp.task('ghost:start', function (callback) {
+    g = ghost({
+        config: path.join(__dirname, '../ghost-dev-config.js')
+    });
+
+    g.then(function (ghostServer) {
+        ghostServer.start();
+    });
+
+    callback();
+});
