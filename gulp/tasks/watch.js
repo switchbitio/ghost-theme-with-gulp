e0bd5181f7a09767ef8658e1d2dc450cd3b42700
Donovan Muller <donovan@switchbit.io>
2016-06-11 01:28:50 +0200

    Fixed double Ghost load

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,14 +1,13 @@
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
 
-gulp.task('ghost', ['ghost:start'], function (callback) {
+gulp.task('ghost', function (callback) {
   runSequence(
     'symlink',
     'ghost:start', // sass (dependency of ghost:start) does not like it when symlink is run async
     function () {
       gulp.watch('app/**/*.hbs', ['browsersync:reload']);
       gulp.watch('app/assets/scss/**/*.scss', ['sass', 'browsersync:reload']);
-      // gulp.watch(config.js, ['jshint']);
 
       return callback;
     }
