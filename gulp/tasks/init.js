6a86e36a9821c2a37f542a5706b005c29976cf97
Donovan Muller <donovan@switchbit.io>
2016-06-11 01:05:52 +0200

    Updated to Ghost 0.8.0

--- gulp/tasks/init.js
+++ gulp/tasks/init.js
@@ -3,7 +3,7 @@ var replace = require('gulp-replace');
 
 gulp.task('init', ['symlink'], function () {
   return gulp
-    .src('node_modules/ghost/core/server/data/default-settings.json', {base: './'})
+    .src('node_modules/ghost/core/server/data/schema/default-settings.json', {base : './'})
     .pipe(replace(/casper/g, 'my-ghost-theme'))
     .pipe(gulp.dest('./'));
 });
