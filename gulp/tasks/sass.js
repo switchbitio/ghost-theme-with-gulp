ff76f2a6d53f4cdd8bdfe2c001f0ac180948ce0e
Donovan Muller <donovan@switchbit.io>
2016-08-13 08:29:27 +0200

    Added theme styling. Upgraded to Ghost 0.9.0.

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,12 +1,16 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var neat = require("bourbon-neat").includePaths;
+var bourbon = require("bourbon").includePaths;
 
 gulp.task('sass', function () {
   return gulp
     .src('app/assets/scss/**/*.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({
+      includePaths: ['sass'].concat(neat).concat(bourbon)
+    }).on('error', sass.logError))
     .pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest('app/assets/css'));
 });
